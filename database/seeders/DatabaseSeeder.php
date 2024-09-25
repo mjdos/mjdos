47f62feb67c407f74291da4dbdc00b5ad85a2feb
Marcelo Santos <mjdos.2014@gmail.com>
2024-09-25 19:02:16 -0300

    Ajustes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UsersTableSeeder::class,
-            RaritySeeder::class,
+            UsersTableSeeder::class
             ]);
     }
 }
